30d7682999c86eb880aa32ee4e859b96afc96f55
anish0714b <swaroopa13132@gmail.com>
2024-08-26 14:41:36 +0530

    added new feature

--- handCanvasFollower.cs
+++ handCanvasFollower.cs
@@ -22,8 +22,18 @@ public class handCanvasFollower : MonoBehaviour
     public Slider rotationZSlider;
 
     private Vector3 lastHandPosition;
-    public float handSpeedThreshold = 0.1f; 
-    public float handSpeedMultiplier = 2f;  
+    public float handSpeedThreshold = 0.1f;
+    public float handSpeedMultiplier = 2f;
+
+    public float autoHideDelay = 5f;  // Time in seconds to auto-hide the canvas
+    private float lastToggleTime;
+
+    public KeyCode saveKey = KeyCode.S;
+    public KeyCode loadKey = KeyCode.L;
+    public string presetName = "DefaultPreset";
+
+    private bool isThumbsUpGesture = false;
+    private bool wasThumbsUpGesture = false;
 
     void Start()
     {
@@ -44,7 +54,9 @@ public class handCanvasFollower : MonoBehaviour
         if (rotationZSlider != null) rotationZSlider.onValueChanged.AddListener(UpdateRotationOffsetZ);
 
         lastHandPosition = targetHandTransform.position;
-        LoadOffsets();  
+        LoadOffsets(presetName);
+
+        lastToggleTime = Time.time;
     }
 
     void Update()
@@ -75,26 +87,40 @@ public class handCanvasFollower : MonoBehaviour
             bool isIndexPinching = leftHand.GetFingerIsPinching(OVRHand.HandFinger.Index);
             if (isIndexPinching && !wasIndexPinching)
             {
-                isCanvasVisible = !isCanvasVisible;
-                canvas.enabled = isCanvasVisible;
-                toggleCount++;
-                Debug.Log("Canvas toggled " + toggleCount + " times.");
+                ToggleCanvasVisibility();
             }
             wasIndexPinching = isIndexPinching;
             lastHandPosition = targetHandTransform.position;
+
+            // Auto-hide canvas after a delay
+            if (isCanvasVisible && Time.time - lastToggleTime > autoHideDelay)
+            {
+                isCanvasVisible = false;
+                canvas.enabled = false;
+                Debug.Log("Canvas auto-hidden after delay.");
+            }
         }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             ToggleCanvasVisibility();
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(saveKey))
         {
-            SaveOffsets();
+            SaveOffsets(presetName);
         }
-        if (Input.GetKeyDown(KeyCode.L))
+        if (Input.GetKeyDown(loadKey))
         {
-            LoadOffsets();
+            LoadOffsets(presetName);
         }
+
+        DetectThumbsUpGesture();
+        if (isThumbsUpGesture && !wasThumbsUpGesture)
+        {
+            ToggleCanvasVisibility();
+            Debug.Log("Canvas toggled with Thumbs Up gesture.");
+        }
+        wasThumbsUpGesture = isThumbsUpGesture;
     }
 
     void UpdatePositionOffsetX(float value)
@@ -144,29 +170,39 @@ public class handCanvasFollower : MonoBehaviour
     {
         isCanvasVisible = !isCanvasVisible;
         canvas.enabled = isCanvasVisible;
-        Debug.Log("Canvas visibility toggled with keyboard.");
+        lastToggleTime = Time.time;
+        Debug.Log("Canvas visibility toggled.");
     }
 
-    public void SaveOffsets()
+    public void SaveOffsets(string preset)
     {
-        PlayerPrefs.SetFloat("PositionOffsetX", positionOffset.x);
-        PlayerPrefs.SetFloat("PositionOffsetY", positionOffset.y);
-        PlayerPrefs.SetFloat("PositionOffsetZ", positionOffset.z);
-        PlayerPrefs.SetFloat("RotationOffsetX", rotationOffset.x);
-        PlayerPrefs.SetFloat("RotationOffsetY", rotationOffset.y);
-        PlayerPrefs.SetFloat("RotationOffsetZ", rotationOffset.z);
+        PlayerPrefs.SetFloat(preset + "_PositionOffsetX", positionOffset.x);
+        PlayerPrefs.SetFloat(preset + "_PositionOffsetY", positionOffset.y);
+        PlayerPrefs.SetFloat(preset + "_PositionOffsetZ", positionOffset.z);
+        PlayerPrefs.SetFloat(preset + "_RotationOffsetX", rotationOffset.x);
+        PlayerPrefs.SetFloat(preset + "_RotationOffsetY", rotationOffset.y);
+        PlayerPrefs.SetFloat(preset + "_RotationOffsetZ", rotationOffset.z);
         PlayerPrefs.Save();
-        Debug.Log("Offsets saved.");
+        Debug.Log("Offsets saved as " + preset);
     }
 
-    public void LoadOffsets()
+    public void LoadOffsets(string preset)
     {
-        positionOffset.x = PlayerPrefs.GetFloat("PositionOffsetX", 0);
-        positionOffset.y = PlayerPrefs.GetFloat("PositionOffsetY", 0);
-        positionOffset.z = PlayerPrefs.GetFloat("PositionOffsetZ", 0.1f);
-        rotationOffset.x = PlayerPrefs.GetFloat("RotationOffsetX", 0);
-        rotationOffset.y = PlayerPrefs.GetFloat("RotationOffsetY", 0);
-        rotationOffset.z = PlayerPrefs.GetFloat("RotationOffsetZ", 0);
-        Debug.Log("Offsets loaded.");
+        positionOffset.x = PlayerPrefs.GetFloat(preset + "_PositionOffsetX", 0);
+        positionOffset.y = PlayerPrefs.GetFloat(preset + "_PositionOffsetY", 0);
+        positionOffset.z = PlayerPrefs.GetFloat(preset + "_PositionOffsetZ", 0.1f);
+        rotationOffset.x = PlayerPrefs.GetFloat(preset + "_RotationOffsetX", 0);
+        rotationOffset.y = PlayerPrefs.GetFloat(preset + "_RotationOffsetY", 0);
+        rotationOffset.z = PlayerPrefs.GetFloat(preset + "_RotationOffsetZ", 0);
+        Debug.Log("Offsets loaded from " + preset);
+    }
+
+    private void DetectThumbsUpGesture()
+    {
+        isThumbsUpGesture = leftHand.GetFingerIsPinching(OVRHand.HandFinger.Thumb) &&
+                            !leftHand.GetFingerIsPinching(OVRHand.HandFinger.Index) &&
+                            !leftHand.GetFingerIsPinching(OVRHand.HandFinger.Middle) &&
+                            !leftHand.GetFingerIsPinching(OVRHand.HandFinger.Ring) &&
+                            !leftHand.GetFingerIsPinching(OVRHand.HandFinger.Pinky);
     }
 }
